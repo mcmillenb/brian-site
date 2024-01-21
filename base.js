1448bbf6e61a69067309abe880d6d59fd4074d2c
Brian McMillen <brianmcmillen1@gmail.com>
2024-01-20 22:47:29 -0500

    load rep from SO api

--- base.js
+++ base.js
@@ -1,3 +1,17 @@
-(function () {
+async function loadRep() {
+  const response = await fetch('https://api.stackexchange.com/2.3/users/2678454?site=stackoverflow');
+  const { items } = await response.json();
+  const rep = items?.[0]?.reputation;
+
+  if (rep) {
+    const stat = Math.round(rep / 100) / 10 + 'k';
+    document.querySelector('#so-card .stat').innerHTML = `Rep: ${stat}`;
+  }
+}
+
+(async function () {
   console.log('The cosmos in a pine cone.');
+
+  loadRep();
+  setInterval(loadRep, 60000);
 })();
